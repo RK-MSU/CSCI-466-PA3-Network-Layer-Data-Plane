79487531fd99f7a59be4f1bc917cf3290ec4453d
River Kelly <75388756+RK-MSU@users.noreply.github.com>
2021-11-01 15:13:19 -0600

    Init simulation1.py

---
+++ simulation1.py
@@ -0,0 +1,55 @@
+# simulation1.py
+import network1
+import link1
+import threading
+from time import sleep
+from rprint import print
+
+
+# configuration parameters
+router_queue_size = 0  # 0 means unlimited
+# give the network sufficient time to transfer all packets before quitting
+simulation_time = 2
+
+if __name__ == '__main__':
+    # keeps track of objects, so we can kill their threads
+    object_L = []
+    
+    # create network nodes
+    client = network1.Host(1)
+    object_L.append(client)
+    server = network1.Host(2)
+    object_L.append(server)
+    router_a = network1.Router(name='A', intf_count=1, max_queue_size=router_queue_size)
+    object_L.append(router_a)
+    
+    # create a Link Layer to keep track of links between network nodes
+    link_layer = link1.LinkLayer()
+    object_L.append(link_layer)
+    
+    # add all the links
+    # link parameters: from_node, from_intf_num, to_node, to_intf_num, mtu
+    link_layer.add_link(link1.Link(client, 0, router_a, 0, 50))
+    link_layer.add_link(link1.Link(router_a, 0, server, 0, 50))
+    
+    # start all the objects
+    thread_L = [threading.Thread(name=object.__str__(), target=object.run) for object in object_L]
+    for t in thread_L:
+        t.start()
+    
+    # create some send events
+    for i in range(3):
+        client.udt_send(2, 'Sample data, Sample data, Sample data, Sample data, Sample data, Sample data, Sample data %d' % i)
+    
+    # give the network sufficient time to transfer all packets before quitting
+    sleep(simulation_time)
+    
+    # join all threads
+    for o in object_L:
+        o.stop = True
+    for t in thread_L:
+        t.join()
+    
+    print("All simulation threads joined")
+
+# EOF
