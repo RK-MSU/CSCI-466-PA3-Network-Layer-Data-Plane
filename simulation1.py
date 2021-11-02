4fd2f081d21b109f0fad87ecf39b091d404b41dd
River Kelly <75388756+RK-MSU@users.noreply.github.com>
2021-11-01 20:13:58 -0600

    Update simulation1.py

--- simulation1.py
+++ simulation1.py
@@ -1,6 +1,6 @@
 # simulation1.py
-import network1
-import link1
+import network1 as network
+import link1 as link
 import threading
 from time import sleep
 from rprint import print
@@ -16,21 +16,21 @@ if __name__ == '__main__':
     object_L = []
     
     # create network nodes
-    client = network1.Host(1)
+    client = network.Host(1)
     object_L.append(client)
-    server = network1.Host(2)
+    server = network.Host(2)
     object_L.append(server)
-    router_a = network1.Router(name='A', intf_count=1, max_queue_size=router_queue_size)
+    router_a = network.Router(name='A', intf_count=1, max_queue_size=router_queue_size)
     object_L.append(router_a)
     
     # create a Link Layer to keep track of links between network nodes
-    link_layer = link1.LinkLayer()
+    link_layer = link.LinkLayer()
     object_L.append(link_layer)
     
     # add all the links
     # link parameters: from_node, from_intf_num, to_node, to_intf_num, mtu
-    link_layer.add_link(link1.Link(client, 0, router_a, 0, 50))
-    link_layer.add_link(link1.Link(router_a, 0, server, 0, 50))
+    link_layer.add_link(link.Link(client, 0, router_a, 0, 50))
+    link_layer.add_link(link.Link(router_a, 0, server, 0, 50))
     
     # start all the objects
     thread_L = [threading.Thread(name=object.__str__(), target=object.run) for object in object_L]
